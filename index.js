acadb0be8908ab3910ba949565e5f31d6aa21548
Go Eun-Seok <gomonk3037@gmail.com>
2019-12-11 02:44:22 +0900

    Use commander instead of prompts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const mkdir = require('mkdirp');
 const axios = require('axios');
-const prompts = require('prompts');
+const program = require('commander');
 const puppeteer = require('puppeteer');
 const ora = require('ora');
 
@@ -19,21 +19,21 @@ const readdir = (dir, cb) => {
   });
 };
 
-const process = (answer, data) => {
+const startProcess = (options, data) => {
   const regex = /url\(([^)]+)\)/g;// find "url(blabla)"
   const parsedUrlStr = data.match(regex);
 
   // url(blabla) => blabla
   const getUrl = str => str.substring(4, str.length - 1);
-  const getFamily = str => rmspace(str).toLowerCase();
-  const getFilename = url => getFamily(answer.family) + '_' + rmspace(url.split('/').pop());
+  const getFont = str => rmspace(str).toLowerCase();
+  const getFilename = url => getFont(options.font) + '_' + rmspace(url.split('/').pop());
 
   const urls = parsedUrlStr.map(getUrl);
 
   const downloadFonts = (cb) => {
     const spinner = ora('Downloading font').start();
 
-    const dir = answer.fontsDir;
+    const dir = options.downloadDir;
      
     const request = async (url, onRequest) => {
       const filename = getFilename(url);
@@ -81,16 +81,16 @@ const process = (answer, data) => {
   const generateStylesheet = (cb) => {
     const spinner = ora('Generating [font].css').start();
 
-    const dir = answer.cssDir;
-    const filename = rmspace(answer.family).toLowerCase() + '.css';
-    const relativeDir = path.relative(answer.cssDir, answer.fontsDir).replace(/\\/g, '/') + '/';
+    const dir = options.cssDir;
+    const filename = rmspace(options.font).toLowerCase() + '.css';
+    const relativeDir = path.relative(options.cssDir, options.downloadDir).replace(/\\/g, '/') + '/';
 
     const stylesheet = 
       data.replace(regex, str => `url(${relativeDir}${getFilename(getUrl(str))})`)
       + '\n'
       + 'body {'
       + '\n'
-      + `  font-family: '${answer.family}', sans-serif;`
+      + `  font-family: '${options.font}', sans-serif;`
       + '\n'
       + '}';
   
@@ -119,21 +119,19 @@ const process = (answer, data) => {
   });
 };
 
-const init = async (questions) => {
-  const answer = await prompts(questions);
-
+const launch = async (options) => {
   const spinner = ora('Prepare to download').start();
   const notFound = () => spinner.fail('Font not found');
 
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`https://fonts.googleapis.com/css?family=${answer.family}:${answer.weights.join(',')}&display=swap`);
+    await page.goto(`https://fonts.googleapis.com/css?family=${options.font}:${options.weights}&display=swap`);
     const data = await page.$eval('body', el => el.textContent);
 
     if (data.indexOf('@font-face') > -1) {
       spinner.succeed();
-      process(answer, data)
+      startProcess(options, data)
     } else {
       notFound();
     }
@@ -144,29 +142,19 @@ const init = async (questions) => {
   }
 };
 
-init([
-  {
-    type: 'text',
-    name: 'family',
-    message: 'Font family',
-    validate: str => str && rmspace(str).length,
-  },
-  {
-    type: 'list',
-    name: 'weights',
-    message: 'Font weights (400,700)',
-    initial: '400',
-  },
-  {
-    type: 'text',
-    name: 'fontsDir',
-    message: 'Dir (fonts will be downloaded)',
-    initial: './',
-  },
-  {
-    type: 'text',
-    name: 'cssDir',
-    message: 'Dir ([font].css output)',
-    initial: './',
-  },
-]);
+program
+  .requiredOption('-f, --font <type>', 'Font you want to own')
+  .option('-w --weights <type>', 'Comma separated font weights', '400')
+  .option('-c --css-dir <type>', 'Dir to output [font].css', './')
+  .option('-d --download-dir <type>', 'Dir to download [font]', './');
+
+program.parse(process.argv);
+
+const options = {
+  font: program.font,
+  weights: program.weights,
+  cssDir: program.cssDir,
+  downloadDir: program.downloadDir,
+};
+
+launch(options);
